0609417ced1d21975cb2913842328405b6e27881
PinguinKa <64411758+PinguinKa@users.noreply.github.com>
2024-12-22 10:57:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def send_heh(message):
     count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "he" * count_heh)
 
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
 # Запуск бота
 bot.polling()
