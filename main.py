9f848fd72a04c336d56cd829c197d593dec87ea2
PinguinKa <64411758+PinguinKa@users.noreply.github.com>
2025-02-16 19:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,5 @@ bot = telebot.TeleBot("ТУТ СЕКРЕТНЫЙ ТОКЕН")
 def send_welcome(message):
     bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
 
-# Обработчик команды '/heh'
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
-
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
-
 # Запуск бота
 bot.polling()
